a0eced11814bf27f0cb2cbb670b34aca4e4efcd8
idnewbie <adhitya.putra1903@gmail.com>
2016-10-04 14:52:47 +0700

    Update Shell Scripting

---
+++ repo.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Adding new indonesian repository (local repository)
+
+echo -e "#########################################################"
+echo -e "##\t\t\t\t\t\t\t##"
+echo -e "##\t       Auto Add Repository Indonesia\t        ##"
+echo -e "##  This tools for auto add repository local Indonesia  ##"
+echo -e "##\t\t\tv.0.0.1 (beta)\t\t\t##"
+echo -e "##\t\t\t\t\t\t\t##"
+echo -e "##########################################################"
+
+ubuntu_kambing(){
+   echo -e "This is repo of kambing.ui.ac.id";
+}
+
+ubuntu_itb(){
+   echo -e "This is repo of itb";
+}
+
+ubuntu_kartolo(){
+   echo -e "This is repo of kartolo";
+}
+
+ubuntu_ubaya(){
+   echo -e "This is repo of ubaya";
+}
+
+ubuntu_all(){
+   echo -e "This is repo of all";
+}
+
+
+if [ "$(id -u)" != "0" ]; then
+    echo "Please run as root."
+else
+    echo "Do you want add new repository?"
+    echo "Avalaible Repository now :"
+    echo -e "1) Kambing UI\t\t4) Ubaya"
+    echo -e "2) ITB\t\t\t5) All Repo"
+    echo -e "3) Kartolo"
+    echo -n "Select your repository for adding: "
+    read repo
+    #echo -e "$repo"
+
+case "$repo" in
+   1)
+     echo -e "You're now add repository from kambing.ui.ac.id";
+     ubuntu_kambing
+     ;;
+   2)
+     echo -e "You're now add repository from ITB"
+     ubuntu_itb
+     ;;
+   3)
+     echo -e "You're now add repository from Kartolo"
+     ubuntu_kartolo
+     ;;
+   4)
+     echo -e "You're now add reposiroty from Ubaya"
+     ubuntu_ubaya
+     ;;
+   5)
+     echo -e "You're now add All Repository"
+     ubuntu_all
+     ;;
+   *)
+     echo -e "Select the correct choice."
+     ;;
+esac
+    echo -e "Done..."
+fi
