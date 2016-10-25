87708eacb16a1853ec41acdc1212e67972bc04da
idnewbie <adhitya.putra1903@gmail.com>
2016-10-25 22:33:00 +0700

    Fixing Option Simple git.sh

--- git.sh
+++ git.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-echo -e "What your file want to upload to github?"
-echo -e "use . (all folder and file) or -A (all file) for all folder or file"
-echo -n "Filename: "
-read file
+echo -e "Simple tools Git"
 
-git add $file
+echo -e "1) Clone Git"
+echo -e "2) Push Git"
 
-echo -n "Commit for this file: "
-read commit
-git commit -m "$commit"
+echo -n "Your choice: "
+read choice
 
-echo -n "Push as origin (default) or another: "
-read origin
+case "$choice" in
+   1)
+      echo -e "Do you want clone some project?"
+      echo -n "Enter URL here: "
+      read url
 
-git push -u "$origin" master
+      git clone $url
+      ;;
+   2)
+      echo -e "What your file want to upload to github?"
+      echo -e "use . (all folder and file) or -A (all file) for all folder or file"
+      echo -n "Filename: "
+      read file
+
+      git add $file
+
+      echo -n "Commit for this file: "
+      read commit
+      git commit -m "$commit"
+
+      echo -n "Push as origin (default) or another: "
+      read origin
+
+      git push -u "$origin" master
+      ;;
+    *)
+      echo -e "Wrong choice"
+      exit 1
+      ;;
+esac
