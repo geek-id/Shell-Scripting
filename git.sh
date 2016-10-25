e2e7243fc3c6ad75a9a7013a93185f8a8764898e
idnewbie <adhitya.putra1903@gmail.com>
2016-10-25 21:56:54 +0700

    Bash script Push to Github

---
+++ git.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo -e "What your file want to upload to github?"
+echo -e "use . or -a for all folder or file"
+echo -n "Filename: "
+read file
+
+git add $file
+
+echo -n "Commit for this file: "
+read commit
+git commit -m "$commit"
+
+echo -n "Push as origin (default) or another: "
+read origin
+
+git push -u "$origin" master
