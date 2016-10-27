16a84106f601154b7e499b92a01a10582141e0c1
idnewbie <adhitya.putra1903@gmail.com>
2016-10-28 02:25:52 +0700

    Adding quit choice menu

--- git.sh
+++ git.sh
@@ -5,6 +5,7 @@ echo -e "Simple tools Git Clone and Push"
 echo -e "1) Git Clone"
 echo -e "2) Git Push"
 
+echo -e "\nUse 'q' for quit"
 echo -n "Your choice: "
 read choice
 
@@ -12,6 +13,7 @@ read choice
 case "$choice" in
    1)
       echo -e "Do you want clone some project?"
+      echo -e "\nUse 'q' for quit"
       echo -n "Enter URL here: "
       read url
       regex='(https?|ssh)://[-A-Za-z0-9\+&@#/%?=~_|!:,.;]*[-A-Za-z0-9\+&@#/%=~_|]'
@@ -19,30 +21,38 @@ case "$choice" in
       if [[ $url =~ $regex ]]; then
          git clone $url
       else
+          if [ $url == 'q' ]; then
+	      exit 0;
+          fi
+
          echo -e "Invalid URL for Git Clone"
       fi
+
       ;;
    2)
       echo -e "What your file want to upload?"
-      echo -e "use . (all folder and file) or -A (all file) for all folder or file"
+      echo -e "use : \n. (all folder and file) \n-A (all file) \nfor all folder or file \nOr you can type filename ex. \"git.sh\" or \"git*\""
       echo -n "Filename: "
       read file
 
       git add $file
-
       echo -n "Commit for this file: "
       read commit
       git commit -m "$commit"
 
       echo -n "Push as origin (default) or another [Press Enter to use default]: "
       read origin
-        
+
       if [ -z "$origin" ];then
          origin="origin"    
          git push -u "$origin" master
       else
          git push -u "$origin" master
       fi
+
+      ;;
+    q)
+      exit 0;
       ;;
     *)
       echo -e "Wrong choice"
